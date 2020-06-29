95c5d3ea97844b0055a0858078eed5e849fbaf9e
Rafayel AZIZYAN <rafayel.azizyan@gmail.com>
2020-06-29 17:59:08 +0200

    tp03 : Maven : profils

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,7 @@
 package dev;
 
+import java.util.ResourceBundle;
+
 import com.github.lalyos.jfiglet.FigletFont;
 
 /**
@@ -10,7 +12,13 @@ public class App
 {
     public static void main( String[] args )
     {
-    	 String asciiArt = FigletFont.convertOneLine("App");
+    	ResourceBundle bundle = ResourceBundle.getBundle("application"); 
+    	String titre = bundle.getString("titre");
+    	String asciiArt = FigletFont.convertOneLine(titre);
     	    System.out.println(asciiArt);
+    	    
+    	 // affichage de l'environnement
+   	    String environnement = ResourceBundle.getBundle("application").getString("environnement");
+    	    System.out.println("Environnement : " + environnement);
     }
 }
